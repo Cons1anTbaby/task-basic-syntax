c1e7196096401267be573bceb1c015d1cd756668
Константин <sakancev66@gmail.com>
2022-03-22 13:41:54 +0500

    Практика. Базовый синтаксис

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,5 +1,22 @@
 export function romanToInteger(str) {
     let result = 0;
+    let mod = 1;
+    const valueDict = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+    for (let i = 0; i < str.length; i++) {
+        if (valueDict[str[i]] < valueDict[str[i + 1]]) {
+            mod = -1;
+        }
+        result += valueDict[str[i]] * mod;
+        mod = 1;
+    }
     /*
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
